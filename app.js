82917a17087b88e81ce24d3349108c9e355c6d6b
Erik Tyler Villoria <evilloria@gmail.com>
2016-09-14 06:41:14 +0000

    express update, add multer to test

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // Metadata Microservice
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/', upload.single(), function (req, res, next) {
+  console.log(req.file); 
+  console.log(req.body);
+});
+ 
+
+app.listen(process.env.PORT, function () {
+  console.log('app listening on port ' + process.env.PORT );
+});
